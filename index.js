d960bec96697c71981516ba84eab6e1b75d14c63
snype <snypeatwork@gmail.com>
2025-02-27 00:07:20 +0545

    Basic Functionality Completed

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-function generateNepaliPasswords(){
+document.getElementById("lengthSlider").addEventListener("input", function() {
+    document.getElementById("lengthValue").textContent = this.value;
+});
+
+document.getElementById("countSlider").addEventListener("input", function() {
+    document.getElementById("countValue").textContent = this.value;
+});
+
+
+function generateNepaliPasswords(charset) {
     const passwordbox = document.getElementById('password');
-
-    var nepaliPasswords = [];
-    var nepaliChars = "अआइईउऊऋएऐओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसह";
-    var nepalisymbols = "१२३४५६७८९०";
-    var englishChars = "abcdefghijklmnopqrstuvwxyz";
-    var englishCharsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var englishNumbers = "1234567890";
-    var englishSpecialChars = "!@#$%^&*()_+";
-    var nepaliVowels = "अआइईउऊऋएऐओऔ";
-    var nepaliConsonants = "कखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसह";
-    var nepaliNumbers = "१२३४५६७८९०";
-    var nepaliSpecialChars = "।॥";
-    var ranjanalipi = "कखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसह";
-    var passwordLength = 8;
-    var passwordCount = 10;
+    let length = document.getElementById("lengthSlider").value;
+    let count = document.getElementById("countSlider").value;
     
-   //mix this and generate random password with math floor and random
-    for (var i = 0; i < passwordCount; i++) {
-        var password = '';
-        for (var j = 0; j < passwordLength; j++) {
-            var random = Math.floor(Math.random() * 4);
-            if (random === 0) {
-                password += nepaliChars.charAt(Math.floor(Math.random() * nepaliChars.length));
-            } else if (random === 1) {
-                password += nepalisymbols.charAt(Math.floor(Math.random() * nepalisymbols.length));
-            } else if (random === 2) {
-                password += englishChars.charAt(Math.floor(Math.random() * englishChars.length));
-            } else if (random === 3) {
-                password += englishCharsUpper.charAt(Math.floor(Math.random() * englishCharsUpper.length));
-            } else if (random === 4) {
-                password += englishNumbers.charAt(Math.floor(Math.random() * englishNumbers.length));
-            } else if (random === 5) {
-                password += englishSpecialChars.charAt(Math.floor(Math.random() * englishSpecialChars.length));
-            }
-        }
-        nepaliPasswords.push(password);
+    let charsets = charset || "";
+    if (document.getElementById("nepaliCharsCheckbox").checked) charsets += "अआइईउऊऋएऐओऔकखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसह";
+    if (document.getElementById("nepaliSymbolsCheckbox").checked) charsets += "१२३४५६७८९०";
+    if (document.getElementById("englishCharsCheckbox").checked) charsets += "abcdefghijklmnopqrstuvwxyz";
+    if (document.getElementById("englishCharsUpperCheckbox").checked) charsets += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (document.getElementById("englishNumbersCheckbox").checked) charsets += "1234567890";
+    if (document.getElementById("englishSpecialCharsCheckbox").checked) charsets += "!@#$%^&*()_+";
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        password += charsets.charAt(Math.floor(Math.random() * charsets.length));
     }
-
-    passwordbox.value = nepaliPasswords[0]; 
-
+    
+    passwordbox.value = password;
 }
+
+function copyToClipboard() {
+    var passwordField = document.getElementById("password");
+    navigator.clipboard.writeText(passwordField.value).then(() => {
+        Swal.fire({
+            icon: 'success',
+            title: 'Copied!',
+            text: 'Password copied to clipboard!',
+            timer: 2000,
+            showConfirmButton: false
+        });
+    }).catch(err => {
+        console.error("Failed to copy: ", err);
+    });
+}
+generateNepaliPasswords();
